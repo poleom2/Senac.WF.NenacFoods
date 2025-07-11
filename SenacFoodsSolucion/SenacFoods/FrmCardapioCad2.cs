bb34834f404bdd55f80e156c57dd3eea497ae185
Miguel <poleom2@gmail.cpm>
2025-07-10 21:57:03 -0300

    hablitar o buton Editar da tela FrmCardapio

--- SenacFoodsSolucion/SenacFoods/FrmCardapioCad2.cs
+++ SenacFoodsSolucion/SenacFoods/FrmCardapioCad2.cs
@@ -12,22 +12,70 @@ namespace SenacFoods
 {
     public partial class FrmCardapioCad2 : Form
     {
+        private CardapioItem _CardapioItem;
+
         public FrmCardapioCad2()
         {
             InitializeComponent();
         }
         public FrmCardapioCad2(CardapioItem cardapioItem)
         {
+            _CardapioItem = cardapioItem;
             InitializeComponent();
+            CarreagarDasoaDaTela();
+
+        }
+
+        private void CarreagarDasoaDaTela()
+        {
+            if (_CardapioItem != null)
+            {
+                txtTitulo.Text = _CardapioItem.Titulo;
+                txtDescricao.Text = _CardapioItem.Descricao;
+                txtPreco.Text = _CardapioItem.Preco.ToString("F2");
+                chkPossuiPreparo.Checked = _CardapioItem.PossuiPreparo;
+
+            }
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            SalvarCardapio();
+            if (_CardapioItem != null)
+            {
+                InserirCardapio();
+            }
+            else
+            {
+                AtualizarCardapio();
+
+            }
+            
 
         }
 
-        private void SalvarCardapio()
+        private void AtualizarCardapio()
+        {
+            using (var banco = new ComandaDBContest())
+            {
+                string Titulo =txtTitulo.Text;
+                string descricao =txtDescricao.Text;
+                decimal.TryParse(txtPreco.Text, out var preco);
+                bool possuiPreparo = chkPossuiPreparo.Checked;
+
+                var cardapioItem = banco.CardapioItems.First(x => x.Id == _CardapioItem.Id);
+                cardapioItem.Titulo = Titulo;
+                cardapioItem.Descricao = descricao;
+                cardapioItem.Preco = preco;
+                cardapioItem.PossuiPreparo = possuiPreparo;
+
+                banco.CardapioItems.Update(cardapioItem);
+                banco.SaveChanges();
+
+
+            }
+        }
+
+        private void InserirCardapio()
         {
             //conectar
             using (var banco = new ComandaDBContest())
